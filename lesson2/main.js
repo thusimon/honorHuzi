7221f5089c034971c2f06f5d93b730ed48cc587e
Lu.Xu <thusimon@gmail.com>
2018-10-18 17:58:15 -0700

    update lesson2

--- lesson2/main.js
+++ lesson2/main.js
@@ -5,16 +5,24 @@ class LikeButton {
     constructor(){
         this.state = {isLiked:false};
     }
+    setState(newState){
+        const oldEle = this.ele;
+        this.state = Object.assign(this.state,newState);
+        // every time call setState, should re-render
+        this.ele = this.render();
+        if (this.updateDom){
+            this.updateDom(oldEle, this.ele);
+        }
+    }
     changeLikeText(){
-        const likeText = this.ele.querySelector('.like-text');
-        this.state.isLiked = !this.state.isLiked;
-        likeText.innerHTML = this.state.isLiked ? 'Cancel':'Like';
+        let curLiked = this.state.isLiked;
+        this.setState({isLiked: !curLiked});
     }
     render(){
         this.ele = document.createElement("div");
         this.ele.innerHTML = `
           <button class='like-btn'>
-            <span class='like-text'>Like</span>
+            <span class='like-text'>${this.state.isLiked ? 'Cancel': 'Like'}</span>
             <span>\uD83D\uDC4D</span>
           </button>
         `;
@@ -24,7 +32,17 @@ class LikeButton {
 }
 
 const wrapper = document.querySelector('.wrapper');
+
 const likeBtn1 = new LikeButton();
+likeBtn1.updateDom = (oldEle, newEle) =>{
+    wrapper.insertBefore(newEle, oldEle);
+    wrapper.removeChild(oldEle);
+};
 wrapper.appendChild(likeBtn1.render());
+
 const likeBtn2 = new LikeButton();
+likeBtn2.updateDom = (oldEle, newEle) =>{
+    wrapper.insertBefore(newEle, oldEle);
+    wrapper.removeChild(oldEle);
+};
 wrapper.appendChild(likeBtn2.render());
